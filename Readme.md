db8c47393b25e6b7906c14fe75ac2da869cc377e
root <root@localhost.localdomain>
2016-02-19 06:09:37 +0000

    add readme and fix bug

---
+++ Readme.md
@@ -0,0 +1,29 @@
+#A Coding Count Tool
+
+##Condition
+
+ - required python2.7
+
+##Usage
+
+ - to analysis current path
+ ```shell
+python code_count.py
+ ```
+
+ - to analysis other path
+ ```shell
+python code_count.py <path>
+
+ ```
+
+##Unsolved Question
+
+ - this python script is only avaible in python2 because of the problem of encoding
+ - welcome to push request to solve the quesion :)
+
+##Auther
+
+ - email:       hsgwpj@gmail.com
+ - blog:        blog.csdn.net/hsgwpj
+ - homepage:    khalil.wang
--- code_count.py
+++ code_count.py
@@ -1,27 +1,30 @@
-#!/bin/python3.5
+#!/bin/python2.7
 import os
+import sys
 
 # Add filetype what you want to scan in the dictionary
-code_sum = {'c':0, 'cpp':0, 'html':0, 'py':0, 'css':0, 'total':0}
+code_sum = {'c':0, 'cpp':0, 'html':0, 'py':0, 'css':0, 'md':0, 'total':0}
 
 def coding_count(path):
-    print("- Scanning " + path)
+    print("- Scanning [dir]" + path)
     curfiles = os.listdir(path)
+    print(curfiles)
 
     for each in curfiles:
-        if os.path.isdir(each):
+
+        if os.path.isdir(path + '/' + each):
             new_path = path + '/' + each    #get new path
             coding_count(new_path)   #do the function recursively
-            continue
 
-        print("- Reading " + each)
-        filetype = each.split('.')[-1]      #get filetype by name
-        for types in code_sum:
-            if filetype == types:
-                fd = open(path + '/' + each, 'r')
-                for eachline in fd:
-                    code_sum[types] += 1
-                    code_sum["total"] += 1
+        else:
+            filetype = each.split('.')[-1]      #get filetype by name
+            for types in code_sum:
+                if filetype == types:
+                    print("- Reading  [file] " + each)
+                    fd = open(path + '/' + each, 'r')
+                    for eachline in fd:
+                        code_sum[types] += 1
+                        code_sum["total"] += 1
 
 def show_result():
     print("---------Scan over----------------")
@@ -37,8 +40,15 @@ def show_result():
 
 
 def main():
-    coding_count(".")
-    show_result()
+    if len(sys.argv) == 1:
+        coding_count(".")
+    else:
+        coding_count(sys.argv[1])
+
+    if code_sum["total"] == 0:
+        print("\n\nOps! You didn't typed any code there!")
+    else:
+        show_result()
 
 if __name__ == "__main__":
     main()
